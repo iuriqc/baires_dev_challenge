5f43c6ba0ccf9a43f885aeefe231935db44d0824
Iuri Queiroz <iuriqc@hotmail.com>
2025-07-28 10:44:27 -0300

    Update terraform variables

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -6,19 +6,19 @@ variable "project_id" {
 variable "region" {
   description = "Google Cloud region"
   type        = string
-  default     = "us-central1"
+  default     = "us-east1"
 }
 
 variable "zone" {
   description = "Google Cloud zone"
   type        = string
-  default     = "us-central1-a"
+  default     = "us-east1-a"
 }
 
 variable "environment" {
   description = "Environment (dev or prd)"
   type        = string
-  default     = "dev"
+  default     = "prd"
   
   validation {
     condition     = contains(["dev", "prd"], var.environment)
